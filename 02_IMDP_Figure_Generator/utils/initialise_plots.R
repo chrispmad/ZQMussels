9262620a566624f78b1b62077cb3b4569a046a90
Phelan <John.Phelan@gov.bc.ca>
2026-04-28 14:10:20 -0700

    na

--- 02_IMDP_Figure_Generator/utils/initialise_plots.R
+++ 02_IMDP_Figure_Generator/utils/initialise_plots.R
@@ -1,155 +1,204 @@
-
-# NOTE: this script uses tmap version 3.3. That is no longer the version available by default when you use install.packages(). The new version completely alters the plots produced.
-# You may need to manually install tmap version 3.3.
-# install.packages("C:/Users/CMADSEN/Downloads/tmap_3.3.tar.gz", repos = NULL, type = "source")
-# This may require that you also install: {widgetframe}.
+# =====================================================
+#                PACKAGE SETUP
+# =====================================================
 
 pacman::p_load(
-  readxl,
-  ggpubr,
-  ggrepel,
-  plotly,
-  ggExtra,
-  ggtext,
-  tidyverse,
-  leaflet,
-  sf,
-  RColorBrewer,
-  openxlsx,
-  scales,
-  lubridate,
-  plotrix,
-  basemaps,
-  ggplot2)
-
-
-#=====================================================
-#                       OPTIONS 
-#=====================================================
-# setwd("C:/Users/CMADSEN/Downloads/LocalR/long_term_projects/ZQMussels")
-my_opts = read_csv(paste0(here::here(),"/Options.csv"))
-
-#Which year should we focus on?
-my.year = my_opts$year
-# my.year = 2025
-
-#Update GIS maps? We probably want this turned on unless you are making fine adjustments to some figures.
-update.gis = FALSE
-
-# Are there any station names we want to change / correct?
-stations.to.change = data.frame(
-  Station = c('Fraser Valley Roving', "Penticton Roving", "Sumas Border", "Keremeos (Hwy 3)"),
-  new_name = c('Lower Mainland', "Penticton", "Sumas", "Keremeos")
+  readxl, ggpubr, ggrepel, plotly, ggExtra, ggtext,
+  tidyverse, leaflet, sf, RColorBrewer, openxlsx,
+  scales, lubridate, plotrix, basemaps, ggplot2
 )
 
+# =====================================================
+#                  OPTIONS
+# =====================================================
 
-#Are there any stations we would like to exclude from the excel-type figures?
-#In 2021, we will exclude:
-if(my.year == 2022){
-  stations.to.include = c("Golden","Dawson Creek","Mt. Robson",
-                          "Osoyoos","Olsen","Penticton Roving",
-                          "Radium","Pacific","Fraser Valley Roving",
-                          "Keremeos")
+my_opts <- read_csv(here::here("Options.csv"))
+
+my.year   <- my_opts$year
+update.gis <- FALSE
+
+# -----------------------------------------------------
+# Station name corrections
+# -----------------------------------------------------
+
+stations.to.change <- tibble(
+  Station  = c("Fraser Valley Roving", "Penticton Roving",
+               "Sumas Border", "Keremeos (Hwy 3)"),
+  new_name = c("Lower Mainland", "Penticton",
+               "Sumas", "Keremeos")
+)
+
+# -----------------------------------------------------
+# Year-specific station configuration
+# -----------------------------------------------------
+
+year_config <- list(
   
-  stations.to.put.aside = c("Scheduled Inspection","Boat Launch - Okanagan",
-                            "Okanagan",
-                            "Penticton Roving - Hwy 33", 
-                            "Penticton Roving - Inspection Event")
+  `2022` = list(
+    include = c("Golden","Dawson Creek","Mt. Robson","Osoyoos",
+                "Olsen","Penticton Roving","Radium","Pacific",
+                "Fraser Valley Roving","Keremeos"),
+    aside = c("Scheduled Inspection","Boat Launch - Okanagan",
+              "Okanagan","Penticton Roving - Hwy 33",
+              "Penticton Roving - Inspection Event"),
+    leaflet = c("Golden","Radium","Olsen","Dawson Creek",
+                "Mt. Robson","Penticton Roving",
+                "Osoyoos","Fraser Valley Roving")
+  ),
+  
+  `2023` = list(
+    include = c("Golden","Radium","Olsen","Yahk","Pacific",
+                "Osoyoos","Hwy 97c","Mt. Robson","Keremeos",
+                "Dawson Creek","Lower Mainland Roving",
+                "Sumas Border","Cutts (Hwy 93)",
+                "Penticton Roving"),
+    aside = c("Scheduled Inspection","Boat Launch - Okanagan",
+              "Okanagan","Penticton Roving - Hwy 33",
+              "Penticton Roving - Inspection Event"),
+    leaflet = c("Golden","Olsen","Dawson Creek","Mt. Robson",
+                "Penticton Roving","Osoyoos",
+                "Lower Mainland Roving","Yahk")
+  ),
+  
+  `2024` = list(
+    include = c("Golden","Radium","Olsen","Yahk","Pacific",
+                "Osoyoos","Hwy 97c","Mt. Robson","Keremeos",
+                "Dawson Creek","Lower Mainland","Sumas Border",
+                "Cutts (Hwy 93)","Penticton","Douglas Crossing"),
+    aside = c("Scheduled Inspection","Boat Launch - Okanagan",
+              "Okanagan","Penticton Roving - Hwy 33",
+              "Penticton Roving - Inspection Event"),
+    leaflet = c("Golden","Olsen","Dawson Creek","Mt. Robson",
+                "Radium","Sumas","Penticton","Osoyoos",
+                "Lower Mainland","Yahk","Pacific","Keremeos",
+                "Hwy 97c","Cutts (Hwy 93)",
+                "Douglas Crossing")
+  ),
+  
+  `2025` = list(
+    include = c("Golden","Radium","Olsen","Yahk","Pacific",
+                "Osoyoos","Hwy 97c","Mt. Robson","Keremeos",
+                "Dawson Creek","Lower Mainland","Sumas Border",
+                "Cutts (Hwy 93)","Penticton","Douglas Crossing"),
+    aside = c("Scheduled Inspection","Boat Launch - Okanagan",
+              "Okanagan","Penticton Roving - Hwy 33",
+              "Penticton Roving - Inspection Event"),
+    leaflet = c("Golden","Olsen","Dawson Creek","Mt. Robson",
+                "Radium","Sumas","Penticton","Osoyoos",
+                "Lower Mainland","Yahk","Pacific","Keremeos",
+                "Hwy 97c","Cutts (Hwy 93)",
+                "Douglas Crossing")
+  )
+)
+
+cfg <- year_config[[as.character(my.year)]]
+
+stations.to.include        <- cfg$include
+stations.to.put.aside      <- cfg$aside
+leaflet.stations.to.include <- cfg$leaflet
+
+# -----------------------------------------------------
+# Station groupings
+# -----------------------------------------------------
+
+permanent.stations <- c("Dawson Creek" ,"Mt. Robson",
+                        "Golden","Olsen","Yahk",
+                        "Osoyoos")
+
+roving.stations <- c("Penticton","Lower Mainland","Sumas",
+                     "Pacific","Cutts (Hwy 93)",
+                     "Douglas Crossing")
+
+part.time.stations <- c("Sumas","Pacific","Cutts (Hwy 93)",
+                        "Radium","Douglas Crossing",
+                        "Hwy 97c","Keremeos")
+
+# -----------------------------------------------------
+# Paths
+# -----------------------------------------------------
+
+my.data.folder <- file.path(my_opts$zqm_figure_local_folder, "data")
+
+my.external.data.folder <- "//SFP.IDIR.BCGOV/S140/S40203/WFC AEB/General/2 SCIENCE - Invasives/AIS_R_Projects/CMadsen_Wdrive/Projects/ZQMussels/data/"
+
+my.output.folder <- file.path(my_opts$zqm_figure_local_folder, "output")
+
+my.external.output.folder <- file.path(
+  my_opts$zqm_figure_output_remote_folder,
+  my.year
+)
+
+zqm.operations.folder <- my_opts$zqm_operations_data_folder
+
+this.years.report.folder <- file.path(
+  my_opts$remote_spatial_data,
+  "Projects/ZQMussels",
+  paste0(my.year, " IMDP Final Report")
+)
+
+# -----------------------------------------------------
+# Mussel fouled tracker
+# -----------------------------------------------------
+
+MusselFouledTracker <- switch(
+  as.character(my.year),
+  "2022" = file.path(zqm.operations.folder,
+                     "Watercraft Inspection Data/2022 data",
+                     "2022 mussel fouled boats tracking sheet.xlsx"),
+  "2023" = file.path(zqm.operations.folder,
+                     "Watercraft Inspection Data/2023 data",
+                     "2023_mussel_fouled_details.xlsx"),
+  "2024" = file.path(zqm.operations.folder,
+                     "Watercraft Inspection Data/2024 data",
+                     "mussel_fouled_summary.xlsx"),
+  "2025" = file.path(zqm.operations.folder,
+                     "Watercraft Inspection Data/2025 data",
+                     "mussel_fouled_summary.xlsx")
+)
+
+# -----------------------------------------------------
+# CBSA notifications
+# -----------------------------------------------------
+
+cbsa_file <- file.path(
+  my.external.output.folder,
+  "GIS Maps and Excel Figures",
+  paste0(my.year, " COS inbox notifications.xlsx")
+)
+
+if (file.exists(cbsa_file)) {
+  cbsa_dat <- read_excel(cbsa_file, sheet = "Filtered list")
 }
-if(my.year == 2023){
-  stations.to.include = c("Golden","Radium","Olsen","Yahk",
-                          "Pacific","Osoyoos","Hwy 97c","Mt. Robson",
-                          "Keremeos","Dawson Creek","Lower Mainland Roving",
-                          "Sumas Border","Cutts (Hwy 93)","Penticton Roving")
-  stations.to.put.aside = c("Scheduled Inspection","Boat Launch - Okanagan",
-                            "Okanagan",
-                            "Penticton Roving - Hwy 33", 
-                            "Penticton Roving - Inspection Event")
+
+# -----------------------------------------------------
+# Knitr + plotting defaults
+# -----------------------------------------------------
+
+knitr::opts_chunk$set(
+  echo = FALSE,
+  warning = FALSE,
+  message = FALSE,
+  fig.width = 8,
+  fig.height = 4
+)
+
+knitr::opts_knit$set(
+  root.dir = file.path(
+    my.external.output.folder,
+    "GIS Maps and Excel Figures",
+    "ExcelFigures"
+  )
+)
+
+my.grey <- "#5f6a6f"
+
+# -----------------------------------------------------
+# Ensure output folder exists
+# -----------------------------------------------------
+
+excel_fig_dir <- knitr::opts_knit$get("root.dir")
+
+if (!dir.exists(excel_fig_dir)) {
+  dir.create(excel_fig_dir, recursive = TRUE)
 }
-if(my.year == 2024){
-  stations.to.include = c("Golden","Radium","Olsen","Yahk",
-                          "Pacific","Osoyoos","Hwy 97c","Mt. Robson",
-                          "Keremeos","Dawson Creek","Lower Mainland",
-                          "Sumas Border","Cutts (Hwy 93)","Penticton",
-                          "Douglas Crossing")
-  stations.to.put.aside = c("Scheduled Inspection","Boat Launch - Okanagan",
-                            "Okanagan",
-                            "Penticton Roving - Hwy 33", 
-                            "Penticton Roving - Inspection Event")
-}
-permanent.stations = c("Golden","Olsen","Yahk",
-                       "Osoyoos","Mt. Robson",
-                       "Dawson Creek")
-
-roving.stations = c("Penticton","Lower Mainland",
-                    "Sumas","Pacific",
-                    "Cutts (Hwy 93)","Douglas Crossing")
-
-part.time.stations = c("Sumas","Pacific","Cutts (Hwy 93)","Radium","Douglas Crossing", "Hwy 97c","Keremeos")
-# Just in case it's different, which stations do we want to show in the 
-# leaflet maps?
-if(my.year == 2022){
-  leaflet.stations.to.include = c("Golden","Radium","Olsen","Dawson Creek",
-                                  "Mt. Robson","Penticton Roving","Osoyoos","Fraser Valley Roving")
-} 
-if(my.year == 2023){
-  leaflet.stations.to.include = c("Golden","Olsen","Dawson Creek","Mt. Robson",
-                                  "Penticton Roving","Osoyoos","Lower Mainland Roving",
-                                  "Yahk")
-}
-if(my.year == 2024){
-  leaflet.stations.to.include = c(
-    "Golden","Olsen","Dawson Creek","Mt. Robson",
-    "Radium","Sumas",
-    "Penticton","Osoyoos","Lower Mainland",
-    "Yahk","Pacific",
-    "Keremeos","Hwy 97c","Cutts (Hwy 93)","Douglas Crossing")
-}
-
-#Data folders
-my.data.folder = paste0(my_opts$zqm_figure_local_folder,"data/")
-my.external.data.folder = paste0(my_opts$remote_spatial_data,"Projects/ZQMussels/data/")
-my.external.data.folder = paste0("//SFP.IDIR.BCGOV/S140/S40203/WFC AEB/General/2 SCIENCE - Invasives/AIS_R_Projects/CMadsen_Wdrive/Projects/ZQMussels/data/")
-
-#Which folder should we put specific output files in?
-my.output.folder = paste0(my_opts$zqm_figure_local_folder,"output/")
-my.external.output.folder = paste0(my_opts$zqm_figure_output_remote_folder,my.year)
-zqm.operations.folder = my_opts$zqm_operations_data_folder
-this.years.report.folder = paste0(my_opts$remote_spatial_data,"Projects/ZQMussels/",my.year," IMDP Final Report/")
-
-#Where is the mussel-fouled tracking sheet?
-if(my.year == 2022){
-  MusselFouledTracker = paste0(my_opts$zqm_operations_data_folder,"Watercraft Inspection Data/", my.year," data/2022 mussel fouled boats tracking sheet.xlsx")
-}
-if(my.year == 2023){
-  MusselFouledTracker = paste0(my_opts$zqm_operations_data_folder,"Watercraft Inspection Data/", my.year," data/2023_mussel_fouled_details.xlsx")
-}
-if(my.year == 2024){
-  MusselFouledTracker = paste0(my_opts$zqm_operations_data_folder,"Watercraft Inspection Data/", my.year," data/mussel_fouled_summary.xlsx")
-}
-
-#Where can we find the CBSA notifications excel sheet for this year?
-if(file.exists(paste0(my.external.output.folder,"/GIS Maps and Excel Figures/",my.year," COS inbox notifications.xlsx"))){
-  cbsa_dat = read_excel(paste0(my.external.output.folder,"/GIS Maps and Excel Figures/",my.year," COS inbox notifications.xlsx"),
-                        sheet = "Filtered list")
-}
-
-
-#These lines below set options for all of the code for the rest of this script. You probably don't need to change any of these.
-knitr::opts_chunk$set(echo = FALSE, warning = FALSE, message = F, fig.width = 8, fig.height = 4)
-knitr::opts_knit$set(root.dir = paste0(my.external.output.folder,"/GIS Maps and Excel Figures/ExcelFigures"))
-
-#Colour for some of the figures:
-my.grey = "#5f6a6f"
-
-#=====================================================
-#                     END OF OPTIONS
-#=====================================================
-
-#If there is no folder for excel figures for the target year in the I: drive, make it now.
-if(!dir.exists(paste0(my.external.output.folder,"/GIS Maps and Excel Figures/ExcelFigures"))){
-  dir.create(paste0(my.external.output.folder,"/GIS Maps and Excel Figures/ExcelFigures/"),
-             recursive = T)
-}
-
 
